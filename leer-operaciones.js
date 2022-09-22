2eb167cb0f3f24320e821fb855a4cfd7ce3e7a3b
AlonsoSoto <alonso.soto@abits.com>
2022-09-22 16:57:04 -0500

    ejemplo leer 1 operacion

--- leer-operaciones.js
+++ leer-operaciones.js
@@ -1,9 +1,14 @@
 const data  = require('./data')
 const url   = String(data.Url).concat(data.OrgId, '/operaciones') 
 const axios = require('axios').default;
+const params = {T: data.Token};
 
-axios.get(url, {
-        params : {T: data.Token}
+axios.get(url, {params})
+    .then(function (response) {
+        const operaciones  = response.data.data
+        console.log('OperacionesEncontradas:', operaciones.length)
+        console.log('Id Primera encontrada: ', operaciones[0].opOperacionId)
+        return axios.get(url.concat('/', operaciones[0].opOperacionId),  {params})
     })
     .then(function (response) {
         console.log(response.data)
