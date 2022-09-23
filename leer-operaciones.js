82964e336b7de6323019262207b482c819f5d13c
AlonsoSoto <alonso.soto@abits.com>
2022-09-23 14:08:41 -0500

    leerOperaciones

--- leer-operaciones.js
+++ leer-operaciones.js
@@ -1,20 +1,21 @@
 const data  = require('./data')
 const url   = String(data.Url).concat(data.OrgId, '/operaciones') 
 const axios = require('axios').default;
-const params = {T: data.Token};
+const headers = {Authorization: 'Bearer '.concat(data.Token)};
 
-axios.get(url, {headers: {Authorization: 'Bearer '.concat(data.Token) }, params:{fechaInicial: '2022-09-01', fechaFinal: '2022-09-30'}})
+axios.get(url, {headers, params:{fechaInicial: '2022-09-01', fechaFinal: '2022-09-30'}})
     .then(function (response) {
         const operaciones  = response.data.data
         console.log('OperacionesEncontradas:', operaciones.length)
         console.log('Id Primera encontrada: ', operaciones[0].opOperacionId)
-        //El servicio me responde las fechas sobre las que consulto
         console.log('El servicio me confirma las fechas sobre las que hice el request:', response.data.query)
-
+        
         operaciones.map(o => {
             console.log('fechaOperacion: ', o.opFecha)
-        })
-        return axios.get(url.concat('/', operaciones[0].opOperacionId),  {params})
+        });
+
+        return axios.get(url.concat('/', operaciones[0].opOperacionId),  {headers});
+
     })
     .then(function (response) {
         console.log(response.data)
