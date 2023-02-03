ed3b57840f4a9b236580b9b72177b3ebf7b336c3
AlonsoSoto <alonso.soto@abits.com>
2023-02-02 19:08:35 -0600

    Ft

--- python3/formulas.py
+++ python3/formulas.py
@@ -1,18 +1,16 @@
   
 import requests;
  
-
-llave = "llave"
-
+llave = "llave" #Hay que poner la llave de la organizacion en cuestion para que funcione
 
 data_json = {
-    "cctos": ["CLOUD"],
+    "cctos": ["CLOUD"], #Pones una lista de los centros de costo a buscar
     "fechaDesde" : "2022-01-01",
     "fechaHasta" : "2022-01-31",
     "incluirCeros": 1,
     "nivel": 5
 }
 
-
+#esta es la balanza x centros de costo
 r = requests.post("http://api.oorden.com/v1/llaves/" + llave+ "/formulasv2/balanza-cctos", json=data_json)
 print( r.json() )
